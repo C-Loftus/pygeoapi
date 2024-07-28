4f682215d0d58341c6542d20d36e709fc8e5d79a
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2024-07-27 23:44:07 -0400

    backup

---
+++ notes/helperscripts.sh
@@ -0,0 +1,14 @@
+
+
+# appear to be 10 unique result types
+# Water
+# Water Quality
+# Biological
+# Environmental
+# Infrastructure and Assets  -> _id = 5
+# Hydropower
+# Safety, Health, and Industrial Hygiene
+# Hydraulic Modeling
+# Water and Environment
+# Environmental Issues for Water
+
